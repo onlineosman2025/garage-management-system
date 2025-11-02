5fda541af6927d53fb2d265692020b46de18ec54
onlineosman2025 <it.osman@alansarschool.net>
2025-11-02 12:21:38 +0400

    Add files via upload

--- simple_server.py
+++ simple_server.py
@@ -646,12 +646,18 @@ class SimpleHandler(http.server.SimpleHTTPRequestHandler):
             self.send_error(404, "Not found")
 
     def do_OPTIONS(self):
+        """Handle preflight CORS requests"""
         self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        self.send_header('Access-Control-Max-Age', '86400')
         self.end_headers()
 
     def send_json(self, data):
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
 
