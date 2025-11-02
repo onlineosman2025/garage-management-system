8132fc094d7a84884b923d3fc48de6b1aaa27176
onlineosman2025 <it.osman@alansarschool.net>
2025-11-02 13:22:54 +0400

    Add files via upload

--- simple_server.py
+++ simple_server.py
@@ -658,7 +658,6 @@ class SimpleHandler(http.server.SimpleHTTPRequestHandler):
     def send_json(self, data):
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
-        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
 
